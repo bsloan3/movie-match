6a5b0636d117d38d914e6f945415515307d61bf7
bradsloan <email": null>
2017-10-27 10:48:54 -0400

    try again with pan to fix errors 2

--- config/environment.rb
+++ config/environment.rb
@@ -8,7 +8,7 @@ require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
 # Require gems we care about
 require 'rubygems'
 
-require 'dotenv'
+# require 'dotenv'
 # Dotenv.load
 
 require 'uri'
